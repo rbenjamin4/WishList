455d2fff3761c5c0ad8485d3fee8f44be60cbacf
Gina Sciacchitano <gbell76767@gmail.com>
2024-01-26 16:52:03 -0500

    added list table and updated newList.js and homepage.js accordingly

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const itemRoutes = require('./itemRoutes');
+const listRoutes = require('./listRoutes');
 
 router.use('/users', userRoutes);
 router.use('/items', itemRoutes);
+router.use('/lists', listRoutes);
 
 module.exports = router;
